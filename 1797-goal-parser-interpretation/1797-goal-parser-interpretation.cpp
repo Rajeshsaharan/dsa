77b4ddf7b71d09db39d1131a2797d9d37debdcb1
Rajesh J Saharan <70120048+Rajeshsaharan@users.noreply.github.com>
2024-12-22 11:01:17 +0530

    Time: 0 ms (100%), Space: 15.8 MB (7.35%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.cpp
@@ -1,23 +1,28 @@
 class Solution {
+private:
+    string helper(string command, int i, string answer) {
+        if (i >= command.size()) {
+            return answer;
+        }
+        if (command[i] == '(' && command[i + 1] == ')') {
+            answer.push_back('o');
+            i = i + 2;
+            return helper(command, i, answer);
+        }else if(command[i] == 'G'){
+            answer.push_back('G');
+            return helper(command, i+1, answer);
+        }else{
+            answer.push_back('a');
+            answer.push_back('l');
+            i = i +4;
+            return helper(command, i, answer);
+        }
+    };
+
 public:
     string interpret(string command) {
-        int size = command.size();
         string answer = "";
-        int i =0;
-        while(i < size){
-            if(command[i] == '(' && command[i+1] == ')'){
-                answer.push_back('o');
-                i = i +2;
-            }else if (command[i] == 'G'){
-                answer.push_back('G');
-                i = i+1;
-            }else{
-                answer.push_back('a');
-                answer.push_back('l');
-                i= i+4;
-            }
-        }
-        return answer;
-        
-    }
+        return helper(command, 0, answer);
+
+    };
 };
